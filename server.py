32e25292c707bdabf65763336cc9bdf5605054ef
Dingo <vito.petrovic1@gmail.com>
2025-03-31 17:09:04 +0200

    Implemented multi-language functionality

--- server.py
+++ server.py
@@ -5,13 +5,34 @@ from smtplib import SMTP
 from flask_wtf import FlaskForm
 from wtforms import StringField,SubmitField, EmailField
 from wtforms.validators import InputRequired, Email, Length
-
-
+from flask_babel import Babel
+from flask_babel import gettext as _
+# 🟢🟢🟢 TO DO: Add cookie consent check before setting session['lang']
 # ###### preden ga javno objaviš:
 # zrihtaj https
 # izklopi debug
 # ######
 #!!!! slike samo naložiš v static/img folder carousel naloada vse
+#! pushaj v svoj branch
+
+# dodaj coockie accept pop up
+################################################
+# 1. Extract all translatable strings into a POT template (overwrite each time)
+# ---------------------------------------------------------------------------
+# pybabel extract -F babel.cfg -o messages.pot . --ignore .venv
+
+# 2. Initialize a new language (only ONCE per language!)
+#    This creates translations/sl/LC_MESSAGES/messages.po
+# ---------------------------------------------------------------------------
+# pybabel init -i messages.pot -d translations -l sl
+
+# 3. Update existing language files after you add new strings (NO init again!)
+# ---------------------------------------------------------------------------
+# pybabel update -i messages.pot -d translations
+
+# 4. Compile translations into .mo files Flask-Babel uses at runtime
+# ---------------------------------------------------------------------------
+# pybabel compile -d translations
 
 class ContactForm(FlaskForm):
     name = StringField('Full Name', validators=[InputRequired()])
@@ -30,9 +51,33 @@ password = os.getenv("PASS")
 gmail = os.getenv("GMAIL")
 secret_key = os.getenv("SECRET_KEY")
 
-
+babel = Babel()
 app = Flask(__name__)
 app.secret_key = secret_key
+app.config["BABEL_DEFAULT_LOCALE"] = "en"
+app.config["BABEL_TRANSLATION_DIRECTORIES"] = "translations"
+
+
+def get_locale():
+    lang = request.view_args.get("lang_code")
+    if lang in ["en","sl","it"]:
+        return lang
+    return "en"
+
+babel.init_app(app, locale_selector=get_locale)
+
+@app.context_processor
+def inject_lang_code():
+    return {
+        'lang_code': get_locale()
+    }
+@app.context_processor
+def utility_processor():
+    def url_for_lang(lang):
+        args = dict(request.view_args or {})
+        args['lang_code'] = lang
+        return url_for(request.endpoint, **args)
+    return dict(url_for_lang=url_for_lang)
 
 def send_email(name,email,phone,message):
     with  SMTP("smtp.gmail.com", port=587) as connection:
@@ -49,19 +94,27 @@ def send_email(name,email,phone,message):
             f"Message: {message}\n" ))
 
 
+@app.route('/')
+def redirect_to_default():
+    lang = request.accept_languages.best_match(["en-GB","sl", "it"])
+    return redirect(url_for('home', lang_code=lang or "en"))
 
-@app.route("/")
-def home():
+@app.route("/debug")
+def debug_lang():
+    return request.headers.get('Accept-Language')
+
+@app.route("/<lang_code>")
+def home(lang_code):
     img_folder = os.path.join(app.static_folder, "img")
     images = [f for f in os.listdir(img_folder) if f.lower().endswith((".png", ".jpg", ".jpeg", ".webp", ".gif"))]
     return render_template("index.html", images=images)
 
-@app.route("/about")
-def about():
+@app.route("/<lang_code>/about")
+def about(lang_code):
     return render_template("about.html")
 
-@app.route("/contact", methods=["GET", "POST"])
-def contact():
+@app.route("/<lang_code>/contact", methods=["GET", "POST"])
+def contact(lang_code):
     form = ContactForm()
     if form.validate_on_submit():
         send_email(
@@ -72,8 +125,8 @@ def contact():
         )
 
         flash("Message Sent Successfully", "Success")
-        return redirect(url_for("contact", msg_snt=True))
-    return render_template("contact.html", form=form, msg_snt=False)
+        return redirect(url_for("contact"))
+    return render_template("contact.html", form=form)
 
 
 
